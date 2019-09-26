d088968e3ec1d2a2be12799aeacd863cad31b37a
Abdul Aliyev <aliyev_abdul@bah.com>
2019-09-26 08:57:54 -0400

    Add proptypes for Congrats component

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given data-testid attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -6,4 +8,14 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-testid='${val}']`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
