740d63d03358f94395e13da3bd8cbea070221440
Abdul Aliyev <aliyev_abdul@bah.com>
2019-09-26 22:50:55 -0400

    Write code and tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,13 @@
-export default (state, action) => {
-  return null;
-}
+import { actionTypes } from '../actions';
+
+const initialState = false;
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+
+    default:
+      return state;
+  }
+};
