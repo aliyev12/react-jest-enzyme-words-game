cbb004322c1a376308d2f07d6568224cc05f67c7
Abdul Aliyev <aliyev_abdul@bah.com>
2019-09-26 21:19:25 -0400

    Tests and code for non-zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -16,7 +16,6 @@ test('does not throw warning with expected props', () => {
   checkProps(GuessedWords, defaultProps);
 });
 
-
 describe('if there are no words', () => {
   let wrapper;
   beforeEach(() => {
@@ -32,9 +31,31 @@ describe('if there are no words', () => {
     const instructions = findByTestAttr(wrapper, 'guess-instructions');
     expect(instructions.text().length).not.toBe(0);
   });
-
 });
 
 describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 }
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
 
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-word');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders guessed words section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length)
+  });
 });
