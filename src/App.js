a99b5f8637b5c28611b38e686b1cd5e39a35d2bd
Abdul Aliyev <aliyev_abdul@bah.com>
2019-09-29 22:26:10 -0400

    Connect app component and test it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './input';
 import ErrorBoundary from './ErrorBoundary';
+import { getSecretWord } from './actions';
 import './App.css';
 
-const App = () => {
+const App = ({ success, guessedWords }) => {
   return (
-    <div className="container">
+    <div className="container" data-testid="app-component">
       <h1>Words Game</h1>
       <ErrorBoundary>
-        <Congrats success={true} />
+        <Congrats success={success} />
+        <Input secretWord={secretWord}/>
         <GuessedWords
-          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+          guessedWords={guessedWords}
         />
       </ErrorBoundary>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = ({ success, guessedWords, secretWord }) => ({
+  success,
+  guessedWords,
+  secretWord
+});
+
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
 import App from './App';
+import { findByTestAttr } from '../test/testUtils';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setupWithMockStore = (initialState = {}) => {
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+  const wrapper = shallow(<App store={store} />).dive();
+  return wrapper;
+};
+
+describe('<App />', () => {
+  test('renders component without error', () => {
+    const wrapper = setupWithMockStore();
+    const component = findByTestAttr(wrapper, 'app-component');
+  });
+
+  test('has access to `success` state', () => {
+    const success = true;
+    const wrapper = setupWithMockStore({ success });
+    const successProp = wrapper.prop('success');
+    expect(successProp).toBe(success);
+  });
+
+  test('has access to `secretWord` state', () => {
+    const secretWord = 'party';
+    const wrapper = setupWithMockStore({ secretWord });
+    const secretWordProp = wrapper.prop('secretWord');
+    expect(secretWordProp).toBe(secretWord);
+  });
+
+  test('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setupWithMockStore({ guessedWords });
+    const guessWordsProp = wrapper.prop('guessedWords');
+    expect(guessWordsProp).toEqual(guessedWords);
+  });
+
+  test('`getSecretWord` action creator is a function on the props', () => {
+    const wrapper = setupWithMockStore();
+    const getSecretWordProp = wrapper.prop('getSecretWord');
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
